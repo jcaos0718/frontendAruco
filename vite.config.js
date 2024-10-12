db185650dc7e017447f68f006eb5467dd8ea88ad
j3k405 <jesteban.cardona@udea.edu.co>
2024-10-12 10:08:16 -0500

    4do  commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Asegúrate de que la carpeta de salida sea correcta
+  },
 })
