a05d31a8e2b7580c0654c141e123b3412f652d24
j3k405 <jesteban.cardona@udea.edu.co>
2024-10-17 10:59:59 -0500

    Urls Register&Login2

--- src/services/authService.jsx
+++ src/services/authService.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 //const API_URL = 'http://localhost:8000/api/auth/';
-const API_URL = 'https://backendaruco.onrender.com/';
+const API_URL = 'https://backendaruco.onrender.com/api/auth/';
 
 export const login = async (email, password) => {
     const response = await axios.post(`${API_URL}login/`, { email, password });
